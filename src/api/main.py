59f914ee38e81015bbb371aa64947a18733eab26
Clemens Wältken <clemens.waltken@microsoft.com>
2024-11-04 17:16:04 +0100

    Get basic response going

--- src/api/main.py
+++ src/api/main.py
@@ -1,9 +1,27 @@
 import uvicorn
-from fastapi import FastAPI
+from fastapi import FastAPI, Header
+from fastapi.responses import StreamingResponse
+from typing import Annotated
+from openai import OpenAI
+import json
 
 from api.models import RootModel
 
 app = FastAPI()
+
+
+def transform_payload_for_openai_compatibility(payload: RootModel):
+    return {
+        "messages": [
+            {
+                "role": message.role,
+                "name": message.name,
+                "content": message.content,
+            }
+            for message in payload.messages
+        ]
+    }
+
 
 @app.get("/")
 async def dummy_200():
@@ -11,8 +29,28 @@ async def dummy_200():
 
 
 @app.post("/")
-async def request(model: RootModel):
-    return {"created": "Ok!"}
+async def request(
+    payload: RootModel,
+    x_github_token: Annotated[str, Header()],
+    github_public_key_signature: Annotated[str, Header()],
+    github_public_key_identifier: Annotated[str, Header()],
+):
+    compatible_payload = transform_payload_for_openai_compatibility(payload)
+    openai = OpenAI(base_url="https://api.githubcopilot.com", api_key=x_github_token)
+    response = openai.chat.completions.create(
+        stream=True,
+        model="gpt-4o",
+        messages=payload.messages,
+    )
+    
+    async def event_generator():
+        for chunk in response:
+            chunk_dict = chunk.to_dict()
+            chunk_str = "data: " + json.dumps(chunk_dict) + "\n\n"
+            yield chunk_str
+        yield "data: [DONE]\n\n"
+
+    return StreamingResponse(event_generator(), media_type="text/event-stream")
 
 
 @app.post("/callback/")
